0f57dbbb586cd30850b21973e951b10342dfc9b8
Agustinn-1002 <agupasten@gmail.com>
2023-05-20 17:22:35 -0300

    demo

--- src/components/header/js/Header.jsx
+++ src/components/header/js/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import BanerOne from '../../../img/banerOne.png'
+import '../style/Header.css'
 
 const Header = () => {
     return(
         <header>
-            <h1>Holaaaaaaaa header</h1>
+            <div className="container bannerOne">
+                <img src={BanerOne} alt="" className="w-100"/>
+            </div>
         </header>
     )
 }
