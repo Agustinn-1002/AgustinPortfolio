61bda42c867c9682a94eb9567ed83959f2696352
Agustinn-1002 <agupasten@gmail.com>
2023-05-27 20:10:22 -0300

    header Responsive

--- src/components/header/js/Header.jsx
+++ src/components/header/js/Header.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import BanerOne from '../../../img/banerOne.png'
 import mobileBannerOne from '../../../img/mobileBannerOne.png'
 import '../style/Header.css'
-import {bannerInfo} from '../../../Info/bannerData'
+import { bannerInfo } from '../../../Info/bannerData'
 
 const Header = () => {
     const ancho = screen.width
-    return(
+    return (
         <header>
-            <div className="container-md p-0">
-                {ancho <= 430 ? 
-                <img src={bannerInfo[0].imageMobile} className="banner" alt=""/>
-                :
-                <img src={bannerInfo[0].imageDesktop} className="banner" alt=""/>
-                }
-            </div>
-            <div>
-                <div>
-                    <h3>{bannerInfo[0].subTitulo}</h3>
-                    <h1>{bannerInfo[0].titulo}</h1>
-                    <p>{bannerInfo[0].descripcion}</p>
+            <div className="container-md p-0 banner">
+                <div className="info text-white">
+                    <div className="textInfo">
+                        <h3 className="fw-light subTitulo">{bannerInfo[1].subTitulo}</h3>
+                        <h1 className="fw-bold titulo">{bannerInfo[1].titulo}</h1>
+                        <p className="fst-italic descripcion">{bannerInfo[1].descripcion}</p>
+                    </div>
+                    <button className="w-25">Ver Proyecto</button>
                 </div>
-                <button>Ver Proyecto</button>
+                {ancho <= 430 ?
+                    <img src={bannerInfo[1].imageMobile} className="banner" alt="" />
+                    :
+                    <img src={bannerInfo[1].imageDesktop} className="banner" alt="" />
+                }
             </div>
         </header>
     )
