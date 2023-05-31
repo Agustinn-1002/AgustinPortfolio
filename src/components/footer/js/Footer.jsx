f0fe70fbd2154d87ca019a7b96bf3f05ee356775
Agustinn-1002 <agupasten@gmail.com>
2023-05-31 11:09:11 -0300

    Footer complete

--- src/components/footer/js/Footer.jsx
+++ src/components/footer/js/Footer.jsx
@@ -6,12 +6,12 @@ import { AiOutlineLinkedin, AiOutlineInstagram, AiOutlineGithub } from "react-ic
 const Footer = () => {
     return (
         <footer className='dataline mx-auto'>
-            <div className='d-flex justify-content-between align-items-center mt-3 flex-lg-column justify-content-lg-center text-center'>
+            <div className='d-flex justify-content-evenly align-items-center mt-3 flex-lg-column justify-content-lg-center text-center'>
                 <img src={logoIso} alt="" className='imglogoIso' />
-                <div className='d-flex flex-column flex-lg-row'>
-                    <a className='linkRedes' href="#">< AiOutlineInstagram className='IconRedes'/> @_aguspasten_</a>
-                    <a className='linkRedes' href="#">< AiOutlineGithub className='IconRedes'/> Agustinn-1002</a>
-                    <a className='linkRedes' href="#">< AiOutlineLinkedin className='IconRedes'/> Mauro Agustin Pasten</a>
+                <div className='d-flex flex-column flex-lg-row align-items-start'>
+                    <a className='linkRedes' href="https://www.instagram.com/_aguspasten_/">< AiOutlineInstagram className='iconRedes'/> <b>@_aguspasten_</b></a>
+                    <a className='linkRedes' href="https://github.com/Agustinn-1002">< AiOutlineGithub className='iconRedes'/><b> Agustinn-1002</b></a>
+                    <a className='linkRedes' href="https://www.linkedin.com/in/mauro-agustin-pasten-81ba57254/">< AiOutlineLinkedin className='iconRedes'/> <b>Mauro Agustin Pasten</b></a>
                 </div>
             </div>
         </footer>
