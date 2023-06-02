0b9930bb4d9a99beec44c7db448fbeadd940798f
Agustinn-1002 <71684855+Agustinn-1002@users.noreply.github.com>
2023-06-01 23:34:20 -0300

    Demo proyects

--- src/components/Proyectos/js/Proyectos.jsx
+++ src/components/Proyectos/js/Proyectos.jsx
@@ -1,30 +1,45 @@
 import React, { useState } from "react";
-import { bannerInfo } from '../../../Info/bannerData'
 import '../style/Proyectos.css'
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { bannerInfo } from '../../../Info/bannerData'
 
 const Proyectos = () => {
-    const [index, setIndex] = useState(0);
-
-    const handleSelect = (selectedIndex) => {
-        setIndex(selectedIndex);
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 2
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1070 },
+            items: 2
+        },
+        tablet: {
+            breakpoint: { max: 1070, min: 464 },
+            items: 1
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
     };
     return (
         <section className="proyectosContain">
-            <Carousel activeIndex={index} onSelect={handleSelect} className="w-75 p-5 ">
+            <Carousel responsive={responsive} containerClass="carousel-container" swipeable={true} infinite={true} autoPlay={true} autoPlaySpeed={2000}>
                 {bannerInfo.map(e =>
-                    <Carousel.Item className="bg-white" style={{borderRadius:'20px'}}>
-                        <img src={e.imagePortada} className='w-100' style={{borderRadius:'20px 20px 0 0'}}/>
+                    <div className="mx-auto bg-white my-4 cardDataPro" style={{ borderRadius: '20px' }}>
+                        <img src={e.imagePortada} className='w-100' style={{ borderRadius: '20px 20px 0 0' }} />
                         <div className="d-flex flex-column align-items-center justify-content-around ">
-                            <div className="w-75 text-center">
+                            <div className="w-75 text-center my-3">
                                 <h1 className="tituloPro">{e.titulo}</h1>
                                 <h2 className="subTituloPro">{e.subTitulo}</h2>
                             </div>
-                            <div className="mt-5 mb-4">
+                            <div className="mt-4 mb-4">
                                 <button className="btnProyects">VER WEB</button>
                             </div>
                         </div>
-                    </Carousel.Item>
+                    </div>
                 )}
             </Carousel>
         </section>
